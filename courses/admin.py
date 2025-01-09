6f98cbc0b356774fbe64d63f5f18eddfffb4b8a9
Naziru Abdussalam Ibrahim <naziruabdussalami@gmail.com>
2025-01-09 16:31:11 +0100

    Assign courses to staff

--- courses/admin.py
+++ courses/admin.py
@@ -2,7 +2,32 @@ from django.contrib import admin
 from .models import Course
 
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at')
+    list_display = ('title', 'staff', 'description', 'created_at')
     search_fields = ('title',)
+
+    def get_queryset(self, request):
+        """
+        Restrict staff users to only see their own courses.
+        """
+        qs = super().get_queryset(request)
+        if request.user.is_staff and not request.user.is_superuser:
+            return qs.filter(staff=request.user)
+        return qs
+
+    def has_change_permission(self, request, obj=None):
+        """
+        Allow staff to only change their own courses.
+        """
+        if obj and not request.user.is_superuser and obj.staff != request.user:
+            return False
+        return super().has_change_permission(request, obj)
+
+    def has_delete_permission(self, request, obj=None):
+        """
+        Allow staff to only delete their own courses.
+        """
+        if obj and not request.user.is_superuser and obj.staff != request.user:
+            return False
+        return super().has_delete_permission(request, obj)
 
 admin.site.register(Course, CourseAdmin)
--- courses/models.py
+++ courses/models.py
@@ -1,8 +1,16 @@
 from django.db import models
+from django.conf import settings
+# from django.contrib.auth.models import User
 
 class Course(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
+    staff = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        null=False,  # This makes it non-nullable
+        default=1,  # Assigns default value (user with ID 1)
+        )
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
